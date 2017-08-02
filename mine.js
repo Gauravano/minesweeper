d92531213524cad92fc90d1e1505548df7ff6470
gauravano <sachdeva.gaurav1997@gmail.com>
2017-08-02 13:43:01 +0530

    neighbours  generated

--- mine.js
+++ mine.js
@@ -17,27 +17,21 @@ var initialization = function(){
 
 var placingMines = function(){
 
-	// for(int i = 0;i<numRows;i++){
-	// 	for(int j=0;j<numCols;j++){
-
-	// 	}
-	// }
-
 	var arr = [];
 
 	var i=0;
 	while(i < numMines){
 		var a = Math.random()*64;
+		a = Math.round(a);
 
 		var p = arr.indexOf(a);
 		
 		if(p == -1){
 			arr.push(a);
 			i++;
-		
 
-		x = Math.round(a%numRows);
-		y = Math.round(a/numRows);  
+		var x = Math.round(a%numRows);
+		var y = Math.round(a/numRows);  
 
 		matrix[x][y] = -1;
 	}
@@ -46,12 +40,184 @@ var placingMines = function(){
 	console.log("matrix after mines : ",matrix);
 };
 
+var generateNeighbourNum = function(){
+	for(var i=0;i<numRows;i++){
+		for(var j=0;j<numCols;j++){
+			var count = 0;
+			
+			if(matrix[i][j] == -1){
+				continue;
+			}
+			else if(i == 0 && j==0){
+				if(matrix[i+1][j] == -1){
+					count++;
+				}
+				if(matrix[i][j+1]== -1){
+					count++;
+				}
+				if(matrix[i+1][j+1]== -1){
+					count++;
+				}
+
+
+			}
+
+			else if(i == 0 && j== numRows-1){
+				if(matrix[i][j-1] == -1){
+					count++;
+				}
+				if(matrix[i+1][j-1]== -1){
+					count++;
+				}
+				if(matrix[i+1][j]== -1){
+					count++;
+				}
+
+			}
+			else if(i == numRows-1 && j== numRows-1){
+				if(matrix[i-1][j] == -1){
+					count++;
+				}
+				if(matrix[i-1][j-1]== -1){
+					count++;
+				}
+				if(matrix[i][j-1]== -1){
+					count++;
+				}
+
+			}
+
+			else if(i == numRows-1 && j== 0){
+				if(matrix[i-1][j] == -1){
+					count++;
+				}
+				if(matrix[i-1][j+1]== -1){
+					count++;
+				}
+				if(matrix[i][j+1]== -1){
+					count++;
+				}
+
+			}
+			else if(i == 0 && j>0 ){
+				if(matrix[i][j-1] == -1){
+					count++;
+				}
+				if(matrix[i][j+1]== -1){
+					count++;
+				}
+				if(matrix[i+1][j-1]== -1){
+					count++;
+				}
+				if(matrix[i+1][j]== -1){
+					count++;
+				}
+				if(matrix[i+1][j+1]== -1){
+					count++;
+				}
+
+			}
+
+			else if(i == numRows-1 && j>0 ){
+				if(matrix[i][j-1] == -1){
+					count++;
+				}
+				if(matrix[i-1][j-1]== -1){
+					count++;
+				}
+				if(matrix[i-1][j]== -1){
+					count++;
+				}
+				if(matrix[i-1][j+1]== -1){
+					count++;
+				}
+				if(matrix[i][j+1]== -1){
+					count++;
+				}
+
+			}
+			else if(i > 0 && j==0 ){
+				if(matrix[i-1][j] == -1){
+					count++;
+				}
+				if(matrix[i-1][j+1]== -1){
+					count++;
+				}
+				if(matrix[i][j+1]== -1){
+					count++;
+				}
+				if(matrix[i+1][j+1]== -1){
+					count++;
+				}
+				if(matrix[i+1][j]== -1){
+					count++;
+				}
+
+			}
+
+			else if(i > 0 && j==numRows-1 ){
+				if(matrix[i-1][j] == -1){
+					count++;
+				}
+				if(matrix[i-1][j-1]== -1){
+					count++;
+				}
+				if(matrix[i][j-1]== -1){
+					count++;
+				}
+				if(matrix[i+1][j-1]== -1){
+					count++;
+				}
+				if(matrix[i+1][j]== -1){
+					count++;
+				}
+
+			}
+			else{
+				if(matrix[i-1][j-1] == -1){
+					count++;
+				}
+				if(matrix[i-1][j]== -1){
+					count++;
+				}
+				if(matrix[i-1][j+1]== -1){
+					count++;
+				}
+				if(matrix[i][j-1]== -1){
+					count++;
+				}
+				if(matrix[i][j+1]== -1){
+					count++;
+				}
+				if(matrix[i+1][j-1]== -1){
+					count++;
+				}
+				if(matrix[i+1][j]== -1){
+					count++;
+				}
+				if(matrix[i+1][j+1]== -1){
+					count++;
+				}
+
+
+			}
+
+			matrix[i][j] = count;											
+
+
+		}
+	}
+
+	console.log("After neighbours",matrix);
+};
+
 
 var pageStart = function(){
 
 	initialization();
 	console.log("matrix status : " ,matrix);
 	placingMines();
+	generateNeighbourNum();
 };
 
 
